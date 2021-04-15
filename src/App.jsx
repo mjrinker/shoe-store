35f8d789d954517eef4855cebf531237cbb056df
Matt Rinker <mrinker@reliaquest.com>
2021-04-15 10:04:49 -0600

    Fetch and display products from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 
 import './App.css';
 import Footer from './Footer';
+import { getProducts } from './services/productService';
 import Header from './Header';
 
 const App = () => {
-/*
- * const renderProduct = (product) => (
- *   <div
- *     className='product'
- *     key={product.id}
- *   >
- *     <a href='/'>
- *       <img
- *         alt={product.name}
- *         src={`/images/${product.image}`}
- *       />
- *       <h3>{product.name}</h3>
- *       <p>${product.price}</p>
- *     </a>
- *   </div>
- * );
- */
+  const [products, setProducts] = useState([]);
+  const [size, setSize] = useState('');
+
+  useEffect(() => {
+    getProducts('shoes').then((response) => setProducts(response));
+  }, []);
+
+  const filteredProducts = size
+    ? products.filter((product) => product.skus.find((sku) => `${sku.size}` === size))
+    : products;
 
   return (
     <>
@@ -30,12 +27,34 @@ const App = () => {
         <main>
           <section id='filters'>
             <label htmlFor='size'>Filter by Size:</label>{' '}
-            <select id='size'>
+            <select
+              id='size'
+              onChange={(event) => setSize(event.target.value)}
+              value={size}
+            >
               <option value=''>All sizes</option>
               <option value='7'>7</option>
               <option value='8'>8</option>
               <option value='9'>9</option>
             </select>
+            {<h4>Showing {filteredProducts.length} of {products.length} products.</h4>}
+          </section>
+          <section id='products'>
+            {filteredProducts.map((product) => (
+              <div
+                className='product'
+                key={product.id}
+              >
+                <a href='/'>
+                  <img
+                    alt={product.name}
+                    src={`/images/${product.image}`}
+                  />
+                  <h3>{product.name}</h3>
+                  <p>${product.price}</p>
+                </a>
+              </div>
+            ))}
           </section>
         </main>
       </div>
